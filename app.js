9cac4a2527f6cc266379ac9142a7a62f7308a0fb
tejprakash <tejprakash1590@gmail.com>
2018-10-05 22:55:24 -0500

    add project page

--- app.js
+++ app.js
@@ -48,6 +48,13 @@ app.get('/about',(req,res) => {
   });
 });
 
+app.get('/project',(req,res) => {
+  res.render('project.hbs',{
+    pageTitle:'Project Page',
+    welcomeMessage:'Welcome to the Project Page'
+  });
+});
+
 app.get('/bad',(req,res) =>{
   res.send({
     errorMessage:'Unable to send'
